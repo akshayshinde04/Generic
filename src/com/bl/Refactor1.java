2d5d9b53a193c3b286d5c54a46ec9edb5a883ce6
unknown <akshayHVshinde04@gmail.com>
2021-11-28 00:18:08 +0530

    Refector1

---
+++ src/com/bl/Refactor1.java
@@ -0,0 +1,23 @@
+package com.bl;
+
+
+public class Refactor1 <T extends Comparable<T>> {
+		T x, y, z;
+
+		public static <T extends Comparable<T>> T findMaximumNumber(T x, T y, T z) {
+			T max = x;
+			if (y.compareTo(max) > 0) {
+				max = y;
+			}
+			if (z.compareTo(max) > 0) {
+				max = z;
+			}
+			return max;
+		}
+
+		public static void main(String[] args) {
+			System.out.println("Maximum  Number using Integer type are: " + findMaximumNumber(25, 75, 53));
+			System.out.println("Maximum  Number using Double type are: " + findMaximumNumber(23.32, 89.67, 45.27));
+			System.out.println("Maximum  String  using String type are: " + findMaximumNumber("Apple", "Banana", "Orange"));
+		}
+	}

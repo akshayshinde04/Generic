cf5244dd587270a3ff08f46784b1e4f4d31cfd34
unknown <akshayHVshinde04@gmail.com>
2021-11-27 17:42:41 +0530

    UC1_MaximumInteger

--- src/com/bl/Maximum.java
+++ src/com/bl/Maximum.java
@@ -1,7 +1,18 @@
 package com.bl;
 
 public class Maximum {
+	public static Integer findMaximumNumber(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Generics Program");
+		System.out.println("Maximum Number:" + findMaximumNumber(26, 96, 52));
 	}
 }
